8f85907c8ec230de67a0b1181458fac5bc3cfb81
Alvian Hutahaean <alvian.hutahaean@geekseat.com.au>
2016-02-03 14:49:05 +0700

    add solution to master

---
+++ OctopusWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OctopusWeb.Startup))]
+namespace OctopusWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
